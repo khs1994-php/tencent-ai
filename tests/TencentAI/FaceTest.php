7393acb0953fdd055df9cdb01acd9f89d52bcb92
khs1994 <khs1994@khs1994.com>
2018-01-20 13:42:15 +0000

    Apply fixes from StyleCI

--- tests/TencentAI/FaceTest.php
+++ tests/TencentAI/FaceTest.php
@@ -7,13 +7,13 @@ class FaceTest extends TestCase
     public function testDetect()
     {
         // code...
-        $config=[
-            "app_id"=>1106560031,
-            "app_key"=> "ZbRY9cf72TbDO0xb"
+        $config = [
+            'app_id' => 1106560031,
+            'app_key' => 'ZbRY9cf72TbDO0xb',
         ];
 
-        $face=new TencentAI\Face($config);
-        $array=$face->detect(getcwd().'/tests/image/ai/tencent/face/wxc.jpg');
-        $this->assertContains('ok',$array['msg']);
+        $face = new TencentAI\Face($config);
+        $array = $face->detect(getcwd().'/tests/image/ai/tencent/face/wxc.jpg');
+        $this->assertContains('ok', $array['msg']);
     }
 }
