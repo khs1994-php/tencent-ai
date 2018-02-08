022a029bf8869a4d283609ee764644e297679caf
khs1994 <khs1994@khs1994.com>
2018-02-08 14:26:18 +0800

    Update [skip ci]

--- tests/Test.php
+++ tests/Test.php
@@ -8,7 +8,6 @@ use TencentAI\Error\TencentAIError;
 $config = [
     'app_id' => 1106560031,
     'app_key' => 'ZbRY9cf72TbDO0xb',
-    'format' => 'json',
 ];
 
 $ai = TencentAI::tencentAI($config);
@@ -18,7 +17,7 @@ $image = __DIR__.'/image/ai/tencent/face/wxc.jpg';
 $image = file_get_contents($image);
 
 try {
-    $output = $ai->face->detect($image);
+    $output = $ai->photo->sticker($image, 20);
 } catch (TencentAIError $e) {
     $output = $e->getArray();
 }
@@ -26,3 +25,5 @@ try {
 // return array
 
 var_dump($output);
+
+file_put_contents('t.jpg', base64_decode($output['data']['image']));
